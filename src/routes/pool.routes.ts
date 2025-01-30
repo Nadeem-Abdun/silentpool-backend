ecc28c1dcd3df61ac1c2046190e2ec3601e22d85
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-01-30 16:58:45 +0530

    Added Pool Routes File

---
+++ src/routes/pool.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { createPool, joinPool, leavePool } from "../controllers/pool.controller.js";
+import authenticateUser from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// Route to create a pool (protected by authentication middleware)
+router.get("/create", authenticateUser, createPool);
+router.post("/join", authenticateUser, joinPool);
+router.post("/leave", authenticateUser, leavePool);
+
+export default router;
