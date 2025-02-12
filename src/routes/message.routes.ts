3522feab0c010afb0a488b6c8d77dff8aabe9e70
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-02-12 16:30:02 +0530

    Updated router import

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import express from "express";
 import { fetchMessages } from "../controllers/message.controller.js";
 import authenticateUser from "../middlewares/auth.middleware.js";
 
-const router = Router();
+const router = express.Router();
 
 // Route to fetch message history for a pool (protected by authentication middleware)
 router.get("/:poolId/messages", authenticateUser, fetchMessages);
