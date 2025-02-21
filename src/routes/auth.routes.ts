cc4b3e1d37e53e6c8cc7e6c32911cc51ee482f0a
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-02-21 18:35:31 +0530

    Added logger logs

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { generateAnonymousIdentity } from "../controllers/auth.controller.js";
+import logger from "../utilities/logger.utilities.js";
 
 const router = express.Router();
 
 // Route to generate anonymous identity
-router.get("/anonymous", generateAnonymousIdentity);
+router.get("/anonymous", (req, res, next) => {
+    logger.info(`Received request for anonymous identity: ${req.method} ${req.originalUrl}`);
+    next();
+}, generateAnonymousIdentity);
 
 export default router;
