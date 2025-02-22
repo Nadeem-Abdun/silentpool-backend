46025713f7a0a1ecf2aff364465a89459497fc39
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-02-22 18:37:44 +0530

    Added logger logs

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { fetchMessages } from "../controllers/message.controller.js";
 import authenticateUser from "../middlewares/auth.middleware.js";
+import logger from "../utilities/logger.utilities.js";
 
 const router = express.Router();
 
 // Route to fetch message history for a pool (protected by authentication middleware)
-router.get("/:poolId/messages", authenticateUser, fetchMessages);
+router.get("/:poolId/messages", authenticateUser, (req, res, next) => {
+    logger.info(`Received request for messages in poolId: ${req.params.poolId}, ${req.method} ${req.originalUrl}`);
+    next();
+}, fetchMessages);
 
 export default router;
