8fb73c6bec761744e321c64f095963937e9dd357
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-02-23 18:38:20 +0530

    Added logger logs

--- src/routes/pool.routes.ts
+++ src/routes/pool.routes.ts
@@ -1,12 +1,23 @@
 import express from "express";
 import { createPool, joinPool, leavePool } from "../controllers/pool.controller.js";
 import authenticateUser from "../middlewares/auth.middleware.js";
+import logger from "../utilities/logger.utilities.js";
 
 const router = express.Router();
 
-// Route to create a pool (protected by authentication middleware)
-router.get("/create", authenticateUser, createPool);
-router.post("/join", authenticateUser, joinPool);
-router.post("/leave", authenticateUser, leavePool);
+router.get("/create", authenticateUser, (req, res, next) => {
+    logger.info(`Request to create pool, ${req.method} ${req.originalUrl}`);
+    next();
+}, createPool);
+
+router.post("/join", authenticateUser, (req, res, next) => {
+    logger.info(`Request to join pool, poolId: ${req.body.poolId}, ${req.method} ${req.originalUrl}`);
+    next();
+}, joinPool);
+
+router.post("/leave", authenticateUser, (req, res, next) => {
+    logger.info(`Request to leave pool, poolId: ${req.body.poolId}, ${req.method} ${req.originalUrl}`);
+    next();
+}, leavePool);
 
 export default router;
