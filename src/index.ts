41fc1c7b8817ad6be64fb9848e2a466991d37f9d
Nadeem-Abdun <nadeemabdun+github@gmail.com>
2025-02-26 19:01:40 +0530

    Added logger logs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectToDatabase from "./database_connections/index.database_connections.js";
 import { initializeWebSocketServer } from "./services/websocket.server.js";
+import logger from "./utilities/logger.utilities.js";
 
 dotenv.config({ path: ".env" });
 
@@ -12,10 +13,10 @@ initializeWebSocketServer(server);
 connectToDatabase()
     .then(() => {
         app.listen(process.env.PORT, () => {
-            console.log(`Server is running on port http://localhost:${process.env.PORT} & DB Connection Successful`);
+            logger.info(`Server is running on port http://localhost:${process.env.PORT} & DB Connection Successful`);
         });
     })
     .catch((error) => {
-        console.error("Error in connecting to database and starting the server" + error);
+        logger.error("Error in connecting to database and starting the server" + error);
         process.exit(1);
     });
